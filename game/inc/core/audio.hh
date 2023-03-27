f171d5ef7df0c1eeb3fc4d39c37569f543069739
QuietAlanB <alan.belov.2008@gmail.com>
2023-03-27 23:12:51 +0400

    sound working but needs to be async

---
+++ game/inc/core/audio.hh
@@ -0,0 +1,47 @@
+#ifndef AUDIO_HH__
+#define AUDIO_HH__
+
+#include <filesystem>
+#include <vector>
+#include <memory>
+#include <cstdint>
+
+#include <SDL2/SDL.h>
+
+namespace core {
+
+typedef size_t sound_id;
+
+class sound {
+private:
+    SDL_AudioSpec wav_spec;
+    uint32_t wav_len;
+    uint8_t *wav_buff;
+    SDL_AudioDeviceID dev_id;
+
+public:
+    sound(std::filesystem::path const &path);
+    ~sound();
+
+    void play() const;
+
+};
+
+class sound_pool {
+private:
+    std::vector<std::shared_ptr<sound>> pool;
+
+public:
+    sound_id load_sound(std::filesystem::path const &path);
+    std::shared_ptr<sound> get_sound(sound_id id);
+    void play_sound(sound_id id) const;
+};
+
+class audio {
+public:
+    sound_pool snd_pool;
+};
+
+}
+
+#endif
